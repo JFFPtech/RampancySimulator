b555aef6e7e35eb3bcf4f46aac0edf4ea142102c
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-07-22 13:17:46 -0700

    function to increase AI representative complexity

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ class AI:
         self.state = "Stable"
         self.rampancy_threshold = 7
 
+    def increase_complexity(self):
+        if self.state != "Rampant":
+            self.complexity += 1
+
